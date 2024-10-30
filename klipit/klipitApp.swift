09891690fc572ac3ae6c3ec6584db97e870a08c5
Kieran Lawrence <kieran.lawrence@wanews.com.au>
2024-10-30 15:25:58 +0800

    Initial Commit

---
+++ klipit/klipitApp.swift
@@ -0,0 +1,17 @@
+//
+//  klipitApp.swift
+//  klipit
+//
+//  Created by Kieran Lawrence on 30/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct klipitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
